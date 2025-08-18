5cafd57b47afe9004ce78d4585fa878ec43b8d0f
idoumbouya <doumbouyaibrahima@gmail.com>
2025-08-18 18:16:36 +0000

    Commands

---
+++ valor-chain.api.Application/Commands/CreateProjectCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace valor_chain.api.Application.Commands;
+
+public class CreateProjectCommand
+{
+    public Guid CompanyId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime PlannedEndDate { get; set; }
+    public string ProjectType { get; set; }
+}
---
+++ valor-chain.api.Application/Commands/CreateUserCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace valor_chain.api.Application.Commands
+{
+    public class CreateUserCommand
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string UserType { get; set; }
+    }
+}
